11cd98aae923d68461468640640451822017cb10
unleashed <kkipkurui123@gmail.com>
2023-03-15 15:55:19 +0300

    update file

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const { library } = require('@fortawesome/fontawesome-svg-core');
+const { fas } = require('@fortawesome/free-solid-svg-icons');
+
+library.add(fas);
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My Webpack Project',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    watchFiles: ['./'],
+    port: 8080,
+  },
+};
